d063fc1df0c48ef55202489d0542886c37782919
Shaun Harrison <shaun.harrison@forgerock.com>
2024-05-02 14:37:35 +0100

    1408: Rework script to generate new number based on major,minor,patch or bug

--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-version="3.0.0"
+versionBump="patch"
+sapigType="ob"
 # Check if sapigType is either core or ob
-sapigType=$(echo "ob" | tr "[:upper:]" "[:lower:]")
+sapigType=$(echo "$sapigType" | tr "[:upper:]" "[:lower:]")
+versionBump=$(echo "$versionBump" | tr "[:upper:]" "[:lower:]")
+
+if [ "$versionBump" != "major" ] && [ "$versionBump" != "minor" ] && [ "$versionBump" != "patch" ] && [ "$versionBump" != "bug" ]; then
+  echo "Warning: versionBump can only be Major | Minor | Patch | Bug "
+  exit 1
+fi
 if [ "$sapigType" != "core" ] && [ "$sapigType" != "ob" ]; then
   echo "Warning: sapigType can only be 'ob' or 'core'"
   exit 1
@@ -14,5 +21,5 @@ else
   echo "OB Components to be built; IG, RCS, RS, RCS-UI, Test Data Initializer, Functional Tests & DCR Conformance Tests"
   components="IG RCS RS RCSUI TDI FUNC DCR"
 fi
-versionToUse=$(./scripts/docker-get-max-rc-version.sh "$components" "$version")
-echo "Creating rc$versionToUse"
+versionToUse=$(./scripts/docker-get-next-version.sh "$components" "$versionBump")
+echo "Use '$versionToUse' in the release workflows"
